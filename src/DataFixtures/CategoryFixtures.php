089a2454fd8c1633e8cb86a0dc5b13aca483b230
gilbertndz <gilbert.ndayizeye@hotmail.fr>
2022-06-03 11:37:46 +0200

    Quête 7 symfony: les fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'Action',
+        'Animation',
+        'Aventure',
+        'Comédie',
+        'Drame',
+        'Fantastique',
+        'Horreur',
+        'Thriller',
+    ];
+    
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::CATEGORIES as $key => $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+        
+        $manager->flush();
+    }
+}
